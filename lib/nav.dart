57fc156fd1219d02342d162e0894ae94a56489d4
Andre Burke <aburke.dev@gmail.com>
2022-11-06 20:59:15 -0700

    added nav widget

---
+++ lib/nav.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/widgets.dart';
+
+class Nav extends StatefulWidget {
+  const Nav({super.key});
+
+  @override
+  State<Nav> createState() => _NavState();
+}
+
+class _NavState extends State<Nav> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}

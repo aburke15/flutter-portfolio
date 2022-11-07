5dde66370310f2cb2f3080ebf5d2d795bded0d0f
Andre Burke <aburke.dev@gmail.com>
2022-11-06 20:21:44 -0700

    added the title widget

--- lib/custom_title.dart
+++ lib/custom_title.dart
@@ -5,29 +5,50 @@ class CustomTitle extends StatelessWidget {
 
   final String _name = 'Andre Burke';
   final String _jobTitle = 'Software Engineer';
+  final Color _textColor = Colors.white;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(50),
-      alignment: Alignment.center,
-      child: Row(
+    return _titleStack(_name, _jobTitle, _textColor);
+  }
+}
+
+Image _titleImage() {
+  return Image.asset(
+    'images/main_image.jpg',
+    width: 600,
+    height: 240,
+    fit: BoxFit.cover,
+  );
+}
+
+Row _titleRow(String name, String jobTitle, Color textColor) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Column(
-            children: [
-              Container(
-                alignment: Alignment.center,
-                child: Text(_name),
-              ),
-              Container(
-                alignment: Alignment.center,
-                child: Text(_jobTitle),
-              )
-            ],
+          Center(
+            child: Text(
+              name,
+              style: TextStyle(color: textColor, fontSize: 22),
+            ),
+          ),
+          Center(
+            child: Text(
+              jobTitle,
+              style: TextStyle(color: textColor, fontSize: 24),
+            ),
           )
         ],
-      ),
-    );
-  }
+      )
+    ],
+  );
+}
+
+Stack _titleStack(String name, String jobTitle, Color textColor) {
+  return Stack(
+      alignment: Alignment.center,
+      children: [_titleImage(), _titleRow(name, jobTitle, textColor)]);
 }
